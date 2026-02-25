4723f79a7bfab19236492475b6bcf19bedd104f6
bart klomp <bartpklomp@gmail.com>
2026-02-25 20:24:13 +0100

    Changed timeout

--- CurlingRinkManagement/projects/common-api/src/lib/authentication/auth.config.ts
+++ CurlingRinkManagement/projects/common-api/src/lib/authentication/auth.config.ts
@@ -7,7 +7,7 @@ export const authCodeFlowConfig: AuthConfig = {
     responseType: 'code',
     scope: 'openid profile email offline_access api',
     showDebugInformation: true,
-    timeoutFactor: 0.01,
+    timeoutFactor: 0.75,
     checkOrigin: false,
     requireHttps: false,
     strictDiscoveryDocumentValidation:false
